06f525981a360791903f18621a9e483edddf8984
Qingyun WU <qingyun@ag2.ai>
2025-09-20 00:38:47 -0700

    add more modules

---
+++ workshop-08232025-data-hack-summit/module8_real_world_example/marketanalysis/scripts/build_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo -e "\033[0;32mBuilding fastagency docker image for linux/amd64 and pushing to registry\033[0m"
+
+# Ensure buildx is set up
+docker buildx create --use 2>/dev/null
+
+# Build and push for linux/amd64
+docker buildx build  --platform linux/amd64 -t marketanalysisacr.azurecr.io/marketanalysis:latest -f docker/Dockerfile . --push
+
+echo -e "\033[0;32mSuccessfully built and pushed fastagency docker image for linux/amd64\033[0m"
